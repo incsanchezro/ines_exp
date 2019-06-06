fe370f6c0b92025d605ce3b933ae00f869728bd1
Ines <incsanchezro@gmail.com>
2019-06-06 10:22:24 -0600

    File for Menna contributions

---
+++ teamlib/Menna.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#!python
+# -*- coding: utf-8 -*-
+"""This is the Teamline.py library which is created for data analysis!"""
+    
+##############################################################
+#Version 0.0.1 from June 7 /2017
+#Amireson team: Andrew Ireson, Ines Sanchez, Seth Amankwaah, Sujan Basnet 
+#University of Saskatchewan
+#School of Environment and Sustainability
+#Global Water Futures - University of Saskatchewan
+
+
+#Importing libraries.
+
+import numpy as np
+
+
+
+def fun ():
+    """
+        -Function which objective is:...
+        -Parameters:
+        xxxx
+
+        -Returns:
+        xxxx
+    """
+    
